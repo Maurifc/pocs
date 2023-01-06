2d7f16aa1a19b41096ba6545663f62a9adc8e26d
maurifcarmo <mauri_vrb@hotmail.com>
2023-01-05 22:36:45 -0300

    Add middle to print requested url

--- cloud-run/profile/index.js
+++ cloud-run/profile/index.js
@@ -3,13 +3,20 @@ import express from 'express'
 
 const app = express()
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+})
+
 app.get('/', async (req, res) => {
   const data = {
     api: 'profile',
     user: null
   }
-  try {    
-    const apiResponse = await axios.get(`${process.env.USER_API_URL}/user/100`)
+  try {
+    const url = `${process.env.USER_API_URL}/user/100`
+    console.log(`Getting user info at ${url}`);
+    const apiResponse = await axios.get(url)
   
     if(res.status !== 200) {
       console.log('Could not retrieve user info')
